cf2438a9a8d861996944bb4eb5aabb99a2e68579
Zhuoyang (Ellen) Li <112480241+EllenLI2000@users.noreply.github.com>
2025-12-17 14:11:31 +0100

    Import dfUpsertSession from dfClient

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-
+import { dfUpsertSession } from "@/lib/dfClient";
 /**
  * =========================
  * STUDENT-EDITABLE ZONE (DATA MODEL)
